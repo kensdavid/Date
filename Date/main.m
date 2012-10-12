f996e428d58b1adf5664a22a701f499e535285f9
Ken David <ken.s.david@gmail.com>
2012-10-11 21:23:35 -0400

    Initial Commit

---
+++ Date/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Date
+//
+//  Created by Kenneth David on 10/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DateAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DateAppDelegate class]));
+    }
+}
